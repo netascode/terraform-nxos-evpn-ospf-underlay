6a1824fe0430f6bd995a08561d02bec8f73de16a
danischm <danischm@cisco.com>
2022-05-30 21:59:48 +0200

    Fix output values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 (unreleased)
+
+- Fix output values
+
 ## 0.1.0
 
 - Initial release
--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "loopback_id" {
   description = "Loopback ID used for OSPF and PIM."
-  value       = length([for lo in module.loopback : lo.id]) > 0 ? [for lo in module.loopback : lo.id][0] : null
+  value       = length([for lo in module.loopback : lo.id]) > 0 ? substr([for lo in module.loopback : lo.id][0], 2, -1) : null
 }
 
 output "pim_loopback_id" {
   description = "Loopback ID used for PIM Anycast RP."
-  value       = length([for lo in module.pim_loopback : lo.id]) > 0 ? [for lo in module.pim_loopback : lo.id][0] : null
+  value       = length([for lo in module.pim_loopback : lo.id]) > 0 ? substr([for lo in module.pim_loopback : lo.id][0], 2, -1) : null
 }
 
 output "vtep_loopback_id" {
   description = "Loopback ID used for VTEP loopbacks."
-  value       = length([for lo in module.vtep_loopback : lo.id]) > 0 ? [for lo in module.vtep_loopback : lo.id][0] : null
+  value       = length([for lo in module.vtep_loopback : lo.id]) > 0 ? substr([for lo in module.vtep_loopback : lo.id][0], 2, -1) : null
 }
 
 output "loopbacks" {
   description = "List of loopback interfaces, one per device."
   value = [for lo in module.loopback : {
     device       = lo.device
-    ipv4_address = lo.ipv4_address
+    ipv4_address = split("/", lo.ipv4_address)[0]
   }]
 }
 
@@ -25,6 +25,6 @@ output "vtep_loopbacks" {
   description = "List of vtep loopback interfaces, one per leaf."
   value = [for lo in module.vtep_loopback : {
     device       = lo.device
-    ipv4_address = lo.ipv4_address
+    ipv4_address = split("/", lo.ipv4_address)[0]
   }]
 }
